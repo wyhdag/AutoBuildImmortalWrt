6ab08c68a4647e414c3aa86f4cfd4b322360b816
wyhdag <88954355+wyhdag@users.noreply.github.com>
2024-12-31 11:20:54 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -8,7 +8,7 @@ uci set network.lan.gateway='10.0.0.253'
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
+NEW_DESCRIPTION=""
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
