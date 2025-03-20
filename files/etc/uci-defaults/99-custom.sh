f4136634662dc241958917372e3cfe79665b9623
wyhdag <88954355+wyhdag@users.noreply.github.com>
2025-03-20 23:16:09 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -31,43 +31,16 @@ ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
 
 # 网络设置
 if [ "$count" -eq 1 ]; then
-   # 单网口设备 类似于NAS模式 动态获取ip模式 具体ip地址取决于上一级路由器给它分配的ip 也方便后续你使用web页面设置旁路由
-   # 单网口设备 不支持修改ip 不要在此处修改ip 
-   uci set network.lan.proto='dhcp'
-elif [ "$count" -gt 1 ]; then
-   # 提取第一个接口作为WAN
-   wan_ifname=$(echo "$ifnames" | awk '{print $1}')
-   # 剩余接口保留给LAN
-   lan_ifnames=$(echo "$ifnames" | cut -d ' ' -f2-)
-   # 设置WAN接口基础配置
-   uci set network.wan=interface
-   # 提取第一个接口作为WAN
-   uci set network.wan.device="$wan_ifname"
-   # WAN接口默认DHCP
-   uci set network.wan.proto='dhcp'
-   # 设置WAN6绑定网口eth0
-   uci set network.wan6=interface
-   uci set network.wan6.device="$wan_ifname"
-   # 更新LAN接口成员
-   # 查找对应设备的section名称
-   section=$(uci show network | awk -F '[.=]' '/\.@?device\[\d+\]\.name=.br-lan.$/ {print $2; exit}')
-   if [ -z "$section" ]; then
-      echo "error：cannot find device 'br-lan'." >> $LOGFILE
-   else
-      # 删除原来的ports列表
-      uci -q delete "network.$section.ports"
-      # 添加新的ports列表
-      for port in $lan_ifnames; do
-         uci add_list "network.$section.ports"="$port"
-      done
-      echo "ports of device 'br-lan' are update." >> $LOGFILE
-   fi
-   # LAN口设置静态IP
+   # 单网口设备 类似于NAS模式，设置静态IP
+   uci set network.lan=interface
    uci set network.lan.proto='static'
-   # 多网口设备 支持修改为别的ip地址
    uci set network.lan.ipaddr='10.0.0.252'
    uci set network.lan.netmask='255.255.255.0'
-   echo "set 10.0.0.252 at $(date)" >> $LOGFILE
+   echo "set static IP 10.0.0.252 at $(date)" >> $LOGFILE
+else
+   echo "No WAN configuration needed. Only LAN is configured." >> $LOGFILE
+fi
+
    # 判断是否启用 PPPoE
    echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
    if [ "$enable_pppoe" = "yes" ]; then
@@ -85,6 +58,7 @@ elif [ "$count" -gt 1 ]; then
       echo "PPPoE is not enabled. Skipping configuration." >> $LOGFILE
    fi
 fi
+
 
 
 # 设置所有网口可访问网页终端
